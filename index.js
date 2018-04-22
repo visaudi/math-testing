1adb5579966037d9be04ad9f48144facd1be7585
Ben Pardo <benpardo@bens-mbp-4.lan>
2018-04-21 18:55:09 -0600

    Calculates tax.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const addNumbers = (a, b) => a + b;
+const calculateTax = (preTaxTotal) => preTaxTotal + preTaxTotal * 0.07;
 
-module.exports = addNumbers;
+module.exports.addNumbers = addNumbers;
+module.exports.calculateTax = calculateTax;
--- index.test.js
+++ index.test.js
@@ -1,7 +1,12 @@
-
-const addNumbers = require("./index"); 
+const {addNumbers} = require("./index");
+const {calculateTax} = require("./index");
 
 test("Adds two numbers", function () {
     expect(addNumbers(1, 2)).toBe(3);
     expect(addNumbers(4, 8)).toBe(12);
+});
+
+test("Calculates sales tax", function () {
+    expect(calculateTax(10)).toBe(10.70);
+    expect(calculateTax(100)).toBe(107);
 });
